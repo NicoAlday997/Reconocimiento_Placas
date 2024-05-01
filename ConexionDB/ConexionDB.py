4a3269c4a7cddfc804635fa8cf3b89de7029f91f
NicolasAlday <alday997@gmail.com>
2024-04-30 23:11:29 -0600

    2.0.0_Nicolas_Entrega3

---
+++ ConexionDB/ConexionDB.py
@@ -0,0 +1,209 @@
+import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
+from datetime import datetime
+import pymysql
+
+
+# Función para crear un círculo en la esquina superior derecha
+def create_circle_image(radius, color, border_color, border_width):
+    size = (radius * 2, radius * 2)
+    image = Image.new("RGBA", size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    draw.ellipse((0, 0, size[0] - 1, size[1] - 1), fill=color, outline=border_color, width=border_width)
+    return ImageTk.PhotoImage(image)
+
+
+# Función para actualizar la fecha y hora actual
+def actualizar_fecha_hora(lbl_fecha_hora):
+    def actualizar():
+        fecha_hora_actual = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        lbl_fecha_hora.config(text=fecha_hora_actual)
+        ventana.after(1000, actualizar)
+
+    actualizar()
+
+
+# Funcion para reiniciar la app
+def reiniciar_aplicacion():
+    ventana.destroy()  # Cierra la ventana actual
+    iniciar_aplicacion()  # Vuelve a abrir la ventana principal
+
+
+# Función para buscar la matrícula en la base de datos
+def boton_buscar(num_placa):
+    if buscar_en_base_de_datos(num_placa):
+        abrir_tercera_ventana(num_placa)
+    else:
+        abrir_cuarta_ventana()
+
+
+def buscar_en_base_de_datos(num_placa):
+    # Realizar la conexión con la base de datos
+    conexion = pymysql.connect(host='localhost', user='root', passwd='1234', db='autoscanid')
+    cursor = conexion.cursor()
+
+    # Realizar la consulta
+    consulta = "SELECT * FROM Placa WHERE Num_placa = %s"
+    cursor.execute(consulta, (num_placa,))
+    resultado = cursor.fetchone()
+
+    # Cerrar la conexión
+    cursor.close()
+    conexion.close()
+
+    # Devolver True si se encuentra la matrícula, False en caso contrario
+    return resultado is not None
+
+
+# Función para abrir la tercera ventana
+def abrir_tercera_ventana(num_placa):
+    # Realizar la conexión con la base de datos
+    conexion = pymysql.connect(host='localhost', user='root', passwd='1234', db='autoscanid')
+    cursor = conexion.cursor()
+
+    # Realizar la consulta
+    nombre_residente = "SELECT Nombre FROM Vehiculo as v INNER JOIN Residente as r ON v.ID_Residente = r.ID_Residente WHERE v.Num_placa = %s"
+    cursor.execute(nombre_residente, (num_placa,))
+    resultado = cursor.fetchone()
+
+    # Cerrar la conexión
+    cursor.close()
+    conexion.close()
+
+    ventana.withdraw()  # Ocultar la ventana actual
+
+    tercera_ventana = tk.Toplevel()
+    tercera_ventana.title("Bienvenida")
+    tercera_ventana.resizable(width=False, height=False)
+
+    #  Obtenemos el largo y  ancho de la pantalla
+    wtotal = ventana.winfo_screenwidth()
+    htotal = ventana.winfo_screenheight()
+
+    #  Guardamos el largo y alto de la ventana
+    wventana = 755
+    hventana = 473
+
+    #  Aplicamos la siguiente formula para calcular donde debería posicionarse
+    pwidth = round(wtotal / 2 - wventana / 2)
+    pheight = round(htotal / 2 - hventana / 2)
+
+    #  Se lo aplicamos a la geometría de la ventana
+    tercera_ventana.geometry(str(wventana) + "x" + str(hventana) + "+" + str(pwidth) + "+" + str(pheight))
+
+    lbl_fecha_hora = tk.Label(tercera_ventana, font=('Arial', 15))
+    lbl_fecha_hora.pack(side="top", pady=10)
+    actualizar_fecha_hora(lbl_fecha_hora)
+
+    circle_image = create_circle_image(30, "green", "black", 2)
+    lblCircle = Label(tercera_ventana, image=circle_image, borderwidth=0)
+    lblCircle.place(x=680, y=20)
+
+    lbl_bienvenida = tk.Label(tercera_ventana, text="BIENVENIDO", font=('Arial', 40))
+    lbl_bienvenida.pack(expand=True)
+
+    lbl_nombre_usuario = tk.Label(tercera_ventana, text=resultado[0], font=('Arial', 30))
+    lbl_nombre_usuario.pack(expand=True)
+
+    tercera_ventana.after(10000, reiniciar_aplicacion)
+    tercera_ventana.mainloop()
+
+
+#Función para abrir la cuarta ventana
+def abrir_cuarta_ventana():
+    ventana.withdraw()  # Ocultar la ventana actual
+
+    cuarta_ventana = tk.Toplevel()
+    cuarta_ventana.title("Rechazado")
+    cuarta_ventana.resizable(width=False, height=False)
+
+    #  Obtenemos el largo y  ancho de la pantalla
+    wtotal = ventana.winfo_screenwidth()
+    htotal = ventana.winfo_screenheight()
+
+    #  Guardamos el largo y alto de la ventana
+    wventana = 755
+    hventana = 473
+
+    #  Aplicamos la siguiente formula para calcular donde debería posicionarse
+    pwidth = round(wtotal/2-wventana/2)
+    pheight = round(htotal/2-hventana/2)
+
+    #  Se lo aplicamos a la geometría de la ventana
+    cuarta_ventana.geometry(str(wventana)+"x"+str(hventana)+"+"+str(pwidth)+"+"+str(pheight))
+
+    lbl_fecha_hora = tk.Label(cuarta_ventana, font=('Arial', 15))
+    lbl_fecha_hora.pack(side="top", pady=10)
+    actualizar_fecha_hora(lbl_fecha_hora)
+
+    circle_image = create_circle_image(30, "red", "black", 2)
+    lblCircle = Label(cuarta_ventana, image=circle_image, borderwidth=0)
+    lblCircle.place(x=680, y=20)
+
+    marco = tk.Frame(cuarta_ventana)
+    marco.pack(expand=True, pady=20)
+
+    lbl_bienvenida = tk.Label(marco, text="No identificado", font=('Arial', 40))
+    lbl_bienvenida.pack(expand=True)
+
+    instruccion = "No se encontró la matricula mostrada"
+    lbl_instruccion = tk.Label(marco, text=instruccion, font=('Arial', 16))
+    lbl_instruccion.pack(expand=True, pady=20)
+
+    cuarta_ventana.after(3000, reiniciar_aplicacion)
+    cuarta_ventana.mainloop()
+
+# Ventana principal
+
+def iniciar_aplicacion():
+    global ventana
+    ventana = tk.Tk()
+    ventana.title("Lector de placas")
+
+    #  Obtenemos el largo y  ancho de la pantalla
+    wtotal = ventana.winfo_screenwidth()
+    htotal = ventana.winfo_screenheight()
+
+    #  Guardamos el largo y alto de la ventana
+    wventana = 755
+    hventana = 473
+
+    #  Aplicamos la siguiente formula para calcular donde debería posicionarse
+    pwidth = round(wtotal / 2 - wventana / 2)
+    pheight = round(htotal / 2 - hventana / 2)
+
+    #  Se lo aplicamos a la geometría de la ventana
+    ventana.geometry(str(wventana) + "x" + str(hventana) + "+" + str(pwidth) + "+" + str(pheight))
+
+    ventana.resizable(width=False, height=False)
+
+    lblInstrucciones = tk.Label(ventana,
+                                text="Por favor acerque su vehiculo a la camara para la lectura de sus placas y mantengase quieto",
+                                font=('Arial', 12), justify="center", wraplength=450)
+    lblInstrucciones.place(relx=0.5, rely=0.1, anchor='n')
+
+    lbl_fecha_hora = tk.Label(ventana, font=('Arial', 15))
+    lbl_fecha_hora.place(relx=0.5, rely=0, anchor='n')
+    actualizar_fecha_hora(lbl_fecha_hora)
+
+    lblInputImage = Label(ventana)
+    lblInputImage.grid(column=0, row=2)
+
+    # Caja de texto donde se inserta manualmente la placa
+    entry_placa = Entry(ventana, font=('Arial', 12), width=20)
+    entry_placa.place(x=275, y=388)
+
+    boton = tk.Button(ventana, text='Buscar Matricula', height='2', width='28', font=('Arial', 12),
+                      command=lambda: boton_buscar(entry_placa.get()), borderwidth=0, relief=tk.RIDGE, bg='gray')
+    boton.place(x=265, y=420)
+
+    circle_image = create_circle_image(30, "red", "black", 2)
+    lblCircle = Label(ventana, image=circle_image, borderwidth=0)
+    lblCircle.place(x=680, y=20)
+
+    ventana.mainloop()
+
+
+# Llamar a la función para iniciar la aplicación
+iniciar_aplicacion()
---
+++ Lector/Lector_Placa_Vehiculo.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+
+
+class LectorPlacas:
+    def __init__(self):
+        pass
+        self.plate_counts = {}
+
+    def preprocess_plate(self, plate):
+        plate_hsv = cv2.cvtColor(plate, cv2.COLOR_BGR2HSV)
+        lower_white = np.array([0, 0, 135])
+        upper_white = np.array([180, 30, 255])
+        mask_white = cv2.inRange(plate_hsv, lower_white, upper_white)
+        return plate_hsv, mask_white
+
+    def process_frame(self, frame):
+        frame_with_roi = frame.copy()
+        height, width, _ = frame.shape
+        roi_x1 = int(width / 3)
+        roi_x2 = int(width * 2 / 3)
+        roi_y1 = int(height / 3)
+        roi_y2 = int(height * 2 / 3)
+        cv2.rectangle(frame_with_roi, (roi_x1, roi_y1), (roi_x2, roi_y2), (0, 255, 0), 2)
+        roi = frame_with_roi[roi_y1:roi_y2, roi_x1:roi_x2]
+        plate_bgr, plate_bin = self.preprocess_plate(roi)
+
+        # Aquí se muestra la placa binarizada
+        cv2.imshow("Binarized Plate", plate_bin)
+
+        plate_text = self.extract_text_from_plate(plate_bin)
+        contours, _ = cv2.findContours(plate_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            x, y, w, h = cv2.boundingRect(contour)
+            aspect_ratio = float(w) / h
+            if 300 < area < 2000 and 2 <= aspect_ratio <= 3:
+                x += roi_x1
+                y += roi_y1
+                cv2.rectangle(frame_with_roi, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if plate_text is not None:
+            self.draw_plate_text(frame_with_roi, plate_text)
+            print("TEXTO: " + plate_text)
+        return frame_with_roi
+
+    def select_roi(self, frame):
+        height, width, _ = frame.shape
+        x1 = int(width / 3)
+        x2 = int(width * 2 / 3)
+        y1 = int(height / 3)
+        y2 = int(height * 2 / 3)
+        return frame[y1:y2, x1:x2]
+
+
+    # def extract_text_from_plate(self, plate):
+    #     if plate.shape[0] >= 36 and plate.shape[1] >= 82:
+    #         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    #         config = "--psm 3"
+    #         text = pytesseract.image_to_string(Image.fromarray(plate), config=config)
+    #         text_filtered = ''.join(c for c in text if c.isalnum() or c == '-')
+    #         if len(text_filtered) >= 9:
+    #             return text_filtered[:9]
+    #     return None
+
+    def extract_text_from_plate(self, plate):
+        if plate.shape[0] >= 36 and plate.shape[1] >= 82:
+            pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+            config = "--psm 3"
+            text = pytesseract.image_to_string(Image.fromarray(plate), config=config)
+            text_filtered = ''.join(c for c in text if c.isalnum() or c == '-')
+            if len(text_filtered) >= 9:
+                text_filtered = text_filtered[:9]
+                if text_filtered in self.plate_counts:
+                    self.plate_counts[text_filtered] += 1
+                    if self.plate_counts[text_filtered] == 4:
+                        print("La placa {} se ha detectado 4 veces.".format(text_filtered))
+                        # Aquí podrías agregar código para una notificación más elaborada
+                else:
+                    self.plate_counts[text_filtered] = 1
+
+                return text_filtered
+
+        return None
+
+
+    def draw_plate_text(self, frame, text):
+        cv2.rectangle(frame, (870, 750), (1070, 850), (0, 0, 0), cv2.FILLED)
+        cv2.putText(frame, text, (900, 810), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    def process_video(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame_with_plate = self.process_frame(frame)
+            cv2.imshow("Vehicle Detection", frame_with_plate)
+            if cv2.waitKey(1) == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    lector_placas = LectorPlacas()
+    lector_placas.process_video("Placas8.mp4")
